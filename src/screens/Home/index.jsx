9ece6e6146525f1892cf50a2222c2789192aba2c
Richard <richard.siqueira@aluno.senai.br>
2024-08-28 13:24:04 -0300

    styles: add styles home and totoro

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,13 +1,14 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import styles from './styles' 
 
 export default function Home() {
   const navigation = useNavigation()
   return (
-    <View>
-      <Text>Home Screen</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Totoro')}>
-        <Text>Go to Totoro</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Screen</Text>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Totoro')}>
+        <Text style={styles.text}>Go to Totoro</Text>
         </TouchableOpacity> 
     </View>
   )
--- src/screens/Totoro/index.jsx
+++ src/screens/Totoro/index.jsx
@@ -1,13 +1,14 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import styles from './styles'
 
 export default function Totoro() {
     const navigation = useNavigation()
   return (
-    <View>
-      <Text>My Friend Totoro</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-        <Text>Back to Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>My Friend Totoro</Text>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
+        <Text style={styles.text}>Back to Home</Text>
         </TouchableOpacity>
     </View>
   )
